df5d5cd756e2d005d1dd9de930809e941ed0835b
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-01-24 18:53:54 -0800

    feat modal and modal btn

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ const textArea = document.getElementById('textAreaID');
 const addNoteBtn = document.getElementById('addNoteButton');
 
 const notesDiv = document.getElementById('notesDiv')
-notesDiv.style.display = 'flex';
-notesDiv.style.flexWrap = 'wrap';
-notesDiv.style.margin = 10 + 'px';
-notesDiv.style.padding = 5 + 'px';
 
 
 addNoteBtn.addEventListener('click', () => {
     if (!textArea.value) return;
-    // console.log(textArea.value.length);
-   
-    const div = document.createElement('div');
-    div.classList.add('divNoteBox');
-    div.innerText = textArea.value;
-    notesDiv.append(div);
 
-    if (textArea.value.length > 50) {
+    const divBox = document.createElement('div');
+    const para = document.createElement('p');
 
+    divBox.classList.add('divNoteBox');
+    para.innerText = textArea.value;
+    
+    divBox.append(para);
+    notesDiv.append(divBox);
+
+    if (textArea.value.length > 30) {
         const btnToModal = document.createElement('button');
-        btnToModal.innerText = 'See more';
+        btnToModal.innerText = 'See more...';
         btnToModal.classList.add('btnToModal');
-        div.append(btnToModal);
+        divBox.append(btnToModal);
+
+        divBox.classList.add('cut-text');
     }
 
     clearText();
@@ -31,3 +31,13 @@ addNoteBtn.addEventListener('click', () => {
 const clearText = () => {
     textArea.value = '';
 }
+
+const seeMoreBtn = () => {
+    let body = document.body;
+    const modalBox = document.createElement('div');
+
+    body.classList.add('modal');
+    modalBox.classList.add('modalContent');
+    modalBox.append(textArea.value);
+}
+seeMoreBtn()
